1d1f5131acfe7cde06e15ad9b13b49b4e29d41b1
Ruslan Potapov <pruslang@gmail.com>
2020-09-30 00:38:21 +0300

    add loader

--- src/main/java/ru/ruslan/Application.java
+++ src/main/java/ru/ruslan/Application.java
@@ -1,9 +1,13 @@
 package ru.ruslan;
 
+import org.apache.commons.lang.StringUtils;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import ru.ruslan.model.Book;
+import ru.ruslan.repository.BookRepository;
+import java.net.URL;
 
 @SpringBootApplication
 public class Application {
@@ -13,8 +17,18 @@ public class Application {
     }
 
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+    CommandLineRunner initDatabase(BookRepository repository) {
+        return args -> {
+            for (int i = 0; i < 100; i++) {
 
+                repository.save(Book.builder().title("title" + i)
+                        .description(StringUtils.repeat("Description ", i))
+                        .author("author" + i)
+                        .isbn("" + i)
+                        .printYear(2020 + i)
+                        .readAlready(false)
+                        .image(new URL("http://example.com/")).build());
+            }
+        };
+    }
 }
