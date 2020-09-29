db9817becf9424ccde8dc8dc471cd09341316d3c
Ruslan Potapov <pruslang@gmail.com>
2020-09-30 00:36:23 +0300

    #refactor

--- src/main/java/ru/ruslan/error/CustomGlobalExceptionHandler.java
+++ src/main/java/ru/ruslan/error/CustomGlobalExceptionHandler.java
@@ -1,17 +1,21 @@
 package ru.ruslan.error;
 
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import ru.ruslan.service.ConstraintService;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.ConstraintViolationException;
 import java.io.IOException;
+import java.util.Map;
 
 @ControllerAdvice
 public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    // Handle the exception
     @ExceptionHandler(BookNotFoundException.class)
     public void springHandleNotFound(HttpServletResponse response) throws IOException {
         response.sendError(HttpStatus.NOT_FOUND.value());
@@ -21,5 +25,7 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
     public void springUnSupportedFieldPatch(HttpServletResponse response) throws IOException {
         response.sendError(HttpStatus.METHOD_NOT_ALLOWED.value());
     }
+
+
 
 }
