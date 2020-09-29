6ac3269c26134f2f81b9830beded4c8839c2a8c5
Ruslan Potapov <pruslang@gmail.com>
2020-09-29 03:11:21 +0300

    add methods

--- src/main/java/ru/ruslan/controller/BookRestController.java
+++ src/main/java/ru/ruslan/controller/BookRestController.java
@@ -1,5 +1,59 @@
 package ru.ruslan.controller;
 
-public class BookRestController {
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.ruslan.model.Book;
+import ru.ruslan.repository.BookRepository;
+import ru.ruslan.service.BookService;
 
+import java.util.List;
+
+@AllArgsConstructor
+public class BookRestController {
+    private final BookRepository repository;
+    private final BookService bookService;
+
+    // Find
+    @GetMapping("/books")
+    List<Book> findAll() {
+        return repository.findAll();
+    }
+
+    /**
+     * Save the book
+     *
+     * @param newBook the new book
+     * @return return 201 status code (that one or more new resources being created)
+     */
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/books")
+    Book newBook(@RequestBody Book newBook) {
+        return bookService.saveBook(newBook);
+    }
+
+    // Find one
+    @GetMapping("/books/{id}")
+    Book findOne(@PathVariable Long id) {
+        return bookService.findOne(id);
+    }
+
+
+    /**
+     * Update the book.
+     *
+     * @param newBook the new book
+     * @param id      the id
+     * @return the book
+     */
+    @PutMapping("/books/{id}")
+    Book editBook(@RequestBody Book newBook, @PathVariable Long id) {
+        return bookService.modification(newBook, id);
+    }
+
+    // update author only
+    @PatchMapping("/books/{id}")
+    Book patch(@PathVariable Long id) {
+        return bookService.updateField(id);
+    }
 }
