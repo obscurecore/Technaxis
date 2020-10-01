c94f6550bdb0645426314d2a7e8600ae29f050c3
Ruslan Potapov <pruslang@gmail.com>
2020-10-01 22:17:46 +0300

    add method to find phrases in all Books with pagination. PostgresFTS

--- src/main/java/ru/ruslan/repository/BookRepository.java
+++ src/main/java/ru/ruslan/repository/BookRepository.java
@@ -1,12 +1,20 @@
 package ru.ruslan.repository;
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import ru.ruslan.model.Book;
 
 import java.util.List;
 
 public interface BookRepository extends CrudRepository<Book, Long> {
+
     List<Book> findAll(Pageable page);
 
+    @Query(value = "SELECT book.id,author,description,image,isbn, print_year,read_already, title FROM book INNER JOIN book_vector bv on book.id = bv.id\n" +
+            "WHERE tsv @@ plainto_tsquery(:phrase) ORDER BY id",
+            countQuery = "SELECT count(*) FROM book",
+            nativeQuery = true)
+    List<Book> findPhrasesInBooksWithPagination(@Param("phrase") String phrase, Pageable pageable);
 }
